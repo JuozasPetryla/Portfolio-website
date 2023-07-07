ff25871a3419d64beae853c0be7de7962decc8ab
JuozasPetryla <itsgrtjuozas@gmail.com>
2023-07-07 16:39:06 +0300

    Starting

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div class="header-logo">Logo</div>
+    <nav class="nav">
+      <ul class="nav-list">
+        <li class="nav-list-item">Home</li>
+        <li class="nav-list-item">About</li>
+        <li class="nav-list-item">Projects</li>
+        <li class="nav-list-item">Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #52b788;
+  font-size: 3.2rem;
+  font-weight: 500;
+  padding: 1.2rem 2.4rem;
+}
+
+.nav-list {
+  display: flex;
+  list-style: none;
+  gap: 2.4rem;
+}
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="landing-page-container">
+    <div class="landing-page-shape-container">
+      <div class="landing-page-shape-right"></div>
+      <div class="landing-page-shape-left"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
