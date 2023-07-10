3f8f200f345cbc9585ca14295db0da60c522c096
JuozasPetryla <itsgrtjuozas@gmail.com>
2023-07-10 15:18:43 +0300

    Adding skill tooltips

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import './main.css'
 import router from './router'
 
 import BaseButton from './components/UI/BaseButton.vue'
-
+import BaseTooltip from './components/UI/BaseTooltip.vue'
 const app = createApp(App)
 
+app.component('base-tooltip', BaseTooltip)
 app.component('base-button', BaseButton)
 
 app.use(router)
